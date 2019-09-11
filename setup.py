082604390b6043fb801c664b5a76736309abc33f
joel <joel.damata@pointclickcare.com>
2019-09-11 01:10:37 -0400

    fix pypi readme

--- setup.py
+++ setup.py
@@ -1,24 +1,33 @@
-from distutils.core import setup
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
 setup(
   name = 'leaderelection',
   packages = ['leaderelection'],
-  version = '0.0.1',
-  license='GPL3',
+  version = '0.0.2',
+  license = 'GPL3',
   description = 'Kubernetes leader election',
+  long_description_content_type = 'text/markdown',
+  long_description = README,
   author = 'Joel Damata',
   author_email = 'joel.damata94@gmail.com',
   url = 'https://github.com/jdamata',
-  download_url = 'https://github.com/jdamata/k8s-leader-election-py/archive/',
+  download_url = 'https://github.com/jdamata/k8s-leader-election-py/archive/0.0.2.tar.gz',
   keywords = ['Kubernetes', 'Controller', 'Leader', 'Election'],
   install_requires=[
           'kubernetes',
-          'python-json-logger',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GPL3 License',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.7',
   ],
